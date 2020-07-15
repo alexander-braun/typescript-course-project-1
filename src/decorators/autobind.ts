f76f3a3e940342749fb961bef181ec73d4a9c609
alexander-braun <alexanderbraun@freenet.de>
2020-07-16 01:41:56 +0200

    splitted app.js into single files/folders

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,12 @@
+//Autobind decorator
+export function autobind(_1: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFunction = originalMethod.bind(this);
+      return boundFunction;
+    },
+  };
+  return adjustedDescriptor;
+}
